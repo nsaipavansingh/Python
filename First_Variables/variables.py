71070332865ff5f0229fdc2d151c810071b14154
nsaipavansingh <nsaipavansingh>
2025-11-26 14:41:03 -0800

    Updated

---
+++ First_Variables/variables.py
@@ -0,0 +1,46 @@
+# syntax
+# variable_name = value
+# = -> assignemnt operator
+
+
+#value = 10
+#value = 4.2  
+#value = "Pavan"
+
+#print(id(value))
+
+
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+list3 = [4, 5, 6]
+
+
+print(id(list1))
+print(id(list2))
+print(id(list3))
+
+
+# type function defines what. data types
+
+value = 10
+value = 4.2  
+value = "Pavan"
+
+print(type(value))
+
+
+#output:
+
+msg = "Python is fun"
+print(msg)  # Python is fun
+
+
+x = "python "
+y = " is "
+z = "fun1 "
+
+print(x+y+z) 
+
+version = "3"
+
+print("Python " + version + " is fun")
