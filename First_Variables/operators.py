20997a1ac60e13da8d72f29aa0877e44acbe16ca
nsaipavansingh <nsaipavansingh>
2025-11-26 16:27:43 -0800

    Operators

---
+++ First_Variables/operators.py
@@ -0,0 +1,23 @@
+# Arithmetic Operators
+
+num1 = 3
+num2 = 2
+
+print(num1+num2) #addition  
+print(num1-num2) #Subtraction
+print(num1*num2) #Multiplication
+print(num1/num2) #quotient
+print(num1//num2) #floor division
+print(num1**num2) #exponentiation
+print(num1%num2) #modulus    
+
+# compound assignment operators
+
+num = 10
+num += 5
+print(num)
+
+num = 10
+num -= 5
+print(num)
+
