ceae6526143d0ecb34d1ba8dbf69295d88180408
nsaipavansingh <nsaipavansingh>
2025-12-24 12:25:32 -0800

    condtional if, elif

---
+++ First_Variables/01_Conditionals/app.py
@@ -0,0 +1,34 @@
+if 5>2:
+    print("5 is greater than 2")
+
+num = -5
+if num > 0:
+    print(f"{num} is a positive number.");
+elif num == 0:
+    print(f"{num} is zero.");
+else:
+    print(f"{num} is a negative number.");
+
+
+
+number = 7
+if number > 20:
+    print("positive number")
+else:
+    print("negative number")
+
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+## 
+age = int(input("enter your age: "))
+nationality = input("enter your nationality: ")
+if age >= 18 and nationality == "Indian":
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
